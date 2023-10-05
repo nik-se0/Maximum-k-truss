b164c1b8a213ce2ffaa9a38df611098719bcaea9
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2023-10-05 09:02:06 +0300

    update

--- samples/main.cpp
+++ samples/main.cpp
@@ -4,117 +4,105 @@ extern "C" {
 }
 #include "graph.h"
 #include "bin_reader.h"
-//#include <sys/time.h>
 #include <omp.h>
 #include <sstream>
 
-#define TIME 0
+#define SUP 1      //Counting support by marking (1) or counting support by crossing (0)
 
-#define AM 1
-#define AI (AM%2+1)%2
-
-#if TIME
 static double timer() {
     struct timeval tp;
     gettimeofday(&tp, NULL);
-    return ((double) (tp.tv_sec) + tp.tv_usec * 1e-6);
+    return ((double)(tp.tv_sec) + tp.tv_usec * 1e-6);
 }
-#endif
 
 extern int th;
 
 int main(int argc, char* argv[])
 {
-    //Обработка комадной строки: (Имя файла)
-    //std::stringstream convert{ argv[1] };
-    //int th; convert >> th;
+    //Command line processing: (File name)
     char* s1 = argv[1];
 
-    s1 = "C:\\tmp\\m12.mtx";
+    cout << "\n\n\topen file: " << s1 << endl;
 
-    //Иницализация графа
+    //Graph Initialization
     crsGraph gr;
     init_graph(&gr);
     read_mtx_to_crs(&gr, s1);
-   
+
+    printf("\a");
     cout << "\tA graph with " << gr.V << " vertices and " << gr.nz / 2 << " edges is loaded\n\n";
+
     sort_adj(&gr);
 
-    //Инициализация доп.структур для k-truss
-    int* EdgeSupport = new int[gr.nz / 2];      // поддержка ребра 
-    Edge* edTo = new Edge[gr.nz / 2];           // массив ребер
-    int* eid = new int[gr.nz];                  // id ребра для вершин
+    //Initialization of additional structures for k-truss
+    int* EdgeSupport = new int[gr.nz / 2];      //edge support
+    Edge* edTo = new Edge[gr.nz / 2];           //array of edges
+    int* eid = new int[gr.nz];                  //edge id for vertices
     for (long i = 0; i < gr.nz / 2; i++) {
         EdgeSupport[i] = 0;
     }
     getEid(&gr, eid, edTo);
 
 
-    int q[5] = { 1,2,4,8,16 };
-    int q0 = 0;
-    while (q0 < 5)
-    { th = q[q0];
-	
-    if (th == 1) 
-    { //Послеовательный алгоритм
-
-        cout << "\tSERIAL:\n";
-        //Подсчет поддержки
-#if TIME
-        double triTime = 0;
-	    double start =  timer();
-#endif
-#if AM
-        SupAM(&gr, eid, EdgeSupport);         //Маркировка смежности  
-#endif
-#if AI
-        SupAI(&gr, edTo, EdgeSupport);        //Пересечение смежности
-#endif
-#if TIME
-        triTime = timer() - start ;
-        cout << "The time support:\t" << (double)triTime << "\n";
-        triTime = 0;
-        start = timer();
-#endif
-        //Подсчет k-truss
-        
-        K_Truss(&gr, EdgeSupport, edTo, eid);
-#if TIME
-        triTime = timer() - start ;
-        cout << "The time K-truss:\t" << (double)triTime << "\n\n";
-#endif
-        //Результат
-        Print_res(gr.nz / 2, EdgeSupport);
-    }
-    else
-    { //Паралленьный алгоритм
-
+    int TH[5] = { 1,2,4,8,16 };
+    int t = 0;
+    while (t < 5) //проход по потокам
+    {
+        th = TH[t];
         for (long i = 0; i < gr.nz / 2; i++) {
             EdgeSupport[i] = 0;
         }
 
-        cout << "\tPARALLEL " << th << " thread:\n";
-        //Подсчет поддержки
-#pragma omp barrier
-        double itime = omp_get_wtime();
-        SupP(&gr, eid, EdgeSupport);
-        double ftime = omp_get_wtime();
-        double exec_time = ftime - itime;
-        cout << "The time support:\t" << exec_time << "\n";
-        //Подсчет k-truss
-#pragma omp barrier
-        double itime1 = omp_get_wtime();
-        PK_Truss(&gr, EdgeSupport, edTo, eid);
-        double ftime1 = omp_get_wtime();
-        double exec_time1 = ftime1 - itime1;
-        cout << "The time K-truss:\t" << exec_time1 << "\n\n";
+        //Sequential algorithm
+        if (th == 1)
+        {
+            printf("\a");
+            cout << "\tSERIAL:\n";
+            double triTime = 0;
+            double start = timer();
+            //Support counting
+#if SUP
+            SupAM(&gr, eid, EdgeSupport);         //Маркировка смежности
+#else
+            SupAI(&gr, edTo, EdgeSupport);        //Пересечение смежности
+#endif
+            triTime = timer() - start;
+            cout << "The time support:\t" << (double)triTime << "\n";
+            triTime = 0;
+            start = timer();
+            //K-truss counting
+            K_Truss(&gr, EdgeSupport, edTo, eid);
 
-        //Результат
+            triTime = timer() - start;
+            cout << "The time K-truss:\t" << (double)triTime << "\n\n";
+        }
+        //Parallel algorithm
+        else
+        {
+            printf("\a");
+            cout << "\tPARALLEL " << th << " thread:\n";
+#pragma omp barrier
+            double itime = omp_get_wtime();
+            //Support counting
+            SupP(&gr, eid, EdgeSupport);
+            double ftime = omp_get_wtime();
+            double exec_time = ftime - itime;
+            printf("\a");
+            cout << "The time support:\t" << exec_time << "\n";
+#pragma omp barrier
+            double itime1 = omp_get_wtime();
+            //K-truss counting
+            PK_Truss(&gr, EdgeSupport, edTo, eid);
+            double ftime1 = omp_get_wtime();
+            double exec_time1 = ftime1 - itime1;
+            printf("\a");
+            cout << "The time K-truss:\t" << exec_time1 << "\n\n";
+        }
+        //Output of the calculation result
+        printf("\a");
         Print_res(gr.nz / 2, EdgeSupport);
-
+        t++;
     }
 
-        q0++;
-    }
-    return 0;
+return 0;
 }
